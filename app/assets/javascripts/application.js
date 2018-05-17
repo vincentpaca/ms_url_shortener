353a7a9621a3b40a62682653f4788960ecb097da
Vincent Paca <vincent.paca@gmail.com>
2018-05-17 11:16:08 +0800

    Switch to Yarn to manage vendor packages. Prettify the forms a bit.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,5 @@
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
+//= require clipboard/dist/clipboard
 //= require_tree .
---
+++ app/assets/javascripts/clipboard_initializer.js
@@ -0,0 +1,3 @@
+document.addEventListener("turbolinks:load", function() {
+  new ClipboardJS('.copy-to-clipboard');
+});
